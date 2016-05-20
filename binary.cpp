16ef90ff319cce9e3f2e1d6032d70100b6473dc0
Dimitris Lamprinos <pkakelas@gmail.com>
2016-05-20 04:47:54 +0300

    Add binary search implementation

---
+++ binary.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int bsearch(int array[], int left, int right, int value) {
+    int middle = (right + left) / 2;
+
+    if (left > right) {
+        return -1;
+    }
+    if (array[middle] == value) {
+        return middle;
+    }
+    else if (array[middle] > value) {
+        bsearch(array, left, right - 1, value);
+    }
+    else {
+        bsearch(array, left + 1, right, value);
+    }
+}
+
+int main() {
+    int array[] = {-3432, -1043, -20,-1 , 1, 3, 5, 6, 7, 9, 15, 200, 4053, 100000};
+    int left = 0, right = (sizeof array / sizeof array[0]);
+    int value = 7;
+
+    while (left <= right) {
+        int middle = (right + left) / 2;
+        if (array[middle] == value) {
+            cout << middle << endl;
+            break;
+        }
+        else if (array[middle] > value) {
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+    }
+
+    cout << bsearch(array, left, right, value) << endl;
+
+    return 0;
+}
