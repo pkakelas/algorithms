72cf2587152114ed4ef68697c9b88de3d4c5b902
Dimitris Lamprinos <pkakelas@gmail.com>
2016-08-29 18:59:32 +0300

    Add bfs with distance calculation

---
+++ graphs/searching/bfs.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+const int MAX_N = 100;
+int dist[MAX_N];
+
+int main() {
+    bool visited[MAX_N] = {false};
+    int dist[MAX_N] = {0};
+    list<int> E[MAX_N];
+    queue<int> Q;
+    int v, u, n, m, s, t;
+
+    freopen("bfs.in", "r", stdin);
+
+    cin >> n >> m;
+    cin >> s >> t;
+
+    for (int i = 0; i < m; ++i) {
+        cin >> u >> v;
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+
+    bool done = false;
+    Q.push(s);
+    while (!Q.empty()) {
+        int node = Q.front();
+        Q.pop();
+        if (t == node) {
+            done = true;
+            cout << "Distance: " << dist[node] << endl;
+            break;
+        }
+        for (auto it = E[node].begin(); it != E[node].end(); ++it) {
+            if (!visited[*it]) {
+                dist[*it] = dist[node] + 1;
+                Q.push(*it);
+            }
+        }
+        visited[node] = true;
+    }
+
+    if (done) {
+        cout << "yes" << endl;
+    }
+    else {
+        cout << "no" << endl;
+    }
+
+    return 0;
+}
