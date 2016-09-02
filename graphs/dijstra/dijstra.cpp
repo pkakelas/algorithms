b20ba8fb7025c060e2f89856635577d29098bc1f
Dimitris Lamprinos <pkakelas@gmail.com>
2016-09-02 21:28:26 +0300

    Rewrite dijstra using std::pair

--- graphs/dijstra/dijstra.cpp
+++ graphs/dijstra/dijstra.cpp
@@ -10,32 +10,15 @@ using namespace std;
 
 int const MAX_N = 100;
 
-template<class T1>
-class Pair {
-public:
-    T1 id;
-    T1 weight;
-
-    Pair(T1 id, T1 weight) : id(id), weight(weight) {}
-};
-
-class PairCompare {
-    public:
-        bool operator() (const Pair<int> &t1, const Pair<int> &t2) const {
-            return t1.weight > t2.weight;
-        }
-};
-
-
-list<Pair<int>> E[MAX_N];
+list<pair<int, int>> E[MAX_N];
 int state[MAX_N];
 
 int main() {
-    priority_queue<Pair<int>, vector<Pair<int>>, PairCompare> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-	pq.push(Pair<int>(1, 6));
-    pq.push(Pair<int>(2, 5));
-    pq.push(Pair<int>(3, 4));
+	pq.push(pair<int, int>(1, 6));
+    pq.push(pair<int, int>(2, 5));
+    pq.push(pair<int, int>(3, 4));
 
     int n, m, u, v, s, t, w, w_max = 0;
     int dist[MAX_N];
@@ -47,41 +30,44 @@ int main() {
 
     for (int i = 0; i < m; ++i) {
         cin >> u >> v >> w;
-        E[u].push_back(Pair<int>(v, w));
-        E[v].push_back(Pair<int>(u, w));
+        E[u].push_back(pair<int, int>(w, v));
+        E[v].push_back(pair<int, int>(w, u));
         w_max = max(w, w_max);
     }
 
+    w_max = w_max * n;
+
     for (int i = 0; i < MAX_N; ++i) {
-        dist[i] = w_max;
+        dist[i] = 1000;
     }
 
     dist[s] = 0;
-	pq.push(Pair<int>(s, 0));
+	pq.push(pair<int, int>(0, s));
     while(!pq.empty()) {
-        Pair<int> current = pq.top();
+        pair<int, int> current = pq.top();
 		pq.pop();
-        cout << "Current: " << current.id << endl;
+        cout << "Current: " << current.second << endl;
         cout << "Checking kids..." << endl;
-        for (auto it = E[current.id].begin(); it != E[current.id].end(); ++it) {
-            Pair<int> node = *it;
-            cout << "Node: " << node.id << ", state: " << state[node.id] << endl;
-            if (state[node.id] == 0) {
-                if (dist[node.id] > dist[current.id] + node.weight) {
-                    cout << "dist[node]: " << dist[node.id] << ", dist[current]: " << dist[current.id] << ", weight: " << node.weight << endl;
-                    dist[node.id] = dist[current.id] + node.weight;
-                    cout << "dist[" << node.id << "]  = dist[" << current.id << "] + " << node.weight << " = " << dist[node.id] << endl;
+        for (auto it = E[current.second].begin(); it != E[current.second].end(); ++it) {
+            pair<int, int> node = *it;
+            cout << "Node: " << node.second << ", state: " << state[node.second] << endl;
+            if (state[node.second] == 0) {
+                cout << "State " << node.second << " " << dist[node.second] << endl;
+                if (dist[node.second] > dist[current.second] + node.first) {
+                    cout << "dist[node]: " << dist[node.second] << ", dist[current]: " << dist[current.second] << ", weight: " << node.first << endl;
+                    dist[node.second] = dist[current.second] + node.first;
+                    cout << "dist[" << node.second << "]  = dist[" << current.second << "] + " << node.first << " = " << dist[node.second] << endl;
                     pq.push(node);
                 }
             }
         }
-        if (current.id == t) {
+        if (current.second == t) {
             cout << "-------- Target found ---------" << endl;
-            cout << "It's shortest path is: " << dist[current.id] << endl;
-            break;
+            cout << "It's shortest path is: " << dist[current.second] << endl;
+            //break;
         }
-        state[current.id] = 1;
-        cout << "Done with current: " << current.id << endl;
+        state[current.second] = 1;
+        cout << "Done with current: " << current.second << endl;
         cout << "Set current's state to permanent (1)" << endl;
     }
     return 0;
