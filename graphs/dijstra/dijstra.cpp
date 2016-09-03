d6ac5dec034a46239a75aa43019df3de5afa3fdc
Dimitris Lamprinos <pkakelas@gmail.com>
2016-09-03 17:27:01 +0300

    Iteration as C++11, change state to bool and use make_pair

--- graphs/dijstra/dijstra.cpp
+++ graphs/dijstra/dijstra.cpp
@@ -11,14 +11,10 @@ using namespace std;
 int const MAX_N = 100;
 
 list<pair<int, int>> E[MAX_N];
-int state[MAX_N];
+bool state[MAX_N];
 
 int main() {
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    pq.push(pair<int, int>(1, 6));
-    pq.push(pair<int, int>(2, 5));
-    pq.push(pair<int, int>(3, 4));
 
     int n, m, u, v, s, t, w, w_max = 0;
     int dist[MAX_N];
@@ -30,29 +26,28 @@ int main() {
 
     for (int i = 0; i < m; ++i) {
         cin >> u >> v >> w;
-        E[u].push_back(pair<int, int>(w, v));
-        E[v].push_back(pair<int, int>(w, u));
+        E[u].push_back(make_pair(w, v));
+        E[v].push_back(make_pair(w, u));
         w_max = max(w, w_max);
     }
 
     w_max = w_max * n;
 
     for (int i = 0; i < MAX_N; ++i) {
-        dist[i] = 1000;
+        dist[i] = w_max;
     }
 
     dist[s] = 0;
-    pq.push(pair<int, int>(0, s));
+    pq.push(make_pair(0, s));
     while(!pq.empty()) {
         pair<int, int> current = pq.top();
         pq.pop();
         cout << "Current: " << current.second << endl;
         cout << "Checking kids..." << endl;
-        for (auto it = E[current.second].begin(); it != E[current.second].end(); ++it) {
-            pair<int, int> node = *it;
+        for (auto it : E[current.second]) {
+            pair<int, int> node = it;
             cout << "Node: " << node.second << ", state: " << state[node.second] << endl;
-            if (state[node.second] == 0) {
-                cout << "State " << node.second << " " << dist[node.second] << endl;
+            if (!state[node.second]) {
                 if (dist[node.second] > dist[current.second] + node.first) {
                     cout << "dist[node]: " << dist[node.second] << ", dist[current]: " << dist[current.second] << ", weight: " << node.first << endl;
                     dist[node.second] = dist[current.second] + node.first;
@@ -66,7 +61,7 @@ int main() {
             cout << "It's shortest path is: " << dist[current.second] << endl;
             break;
         }
-        state[current.second] = 1;
+        state[current.second] = true;
         cout << "Done with current: " << current.second << endl;
         cout << "Set current's state to permanent (1)" << endl;
     }
